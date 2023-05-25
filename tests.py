08f99d06129fef8d2b6f97eb2d18c152428b586c
Iya Volkova <iyavolkova57@gmail.com>
2023-05-25 07:04:37 +0300

    add test_own_recomendation, test_alg_sell_buy, test_check_alg

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 import unittest
+
+import pytest
+
 from main import *
 from unittest.mock import patch, MagicMock
 from io import StringIO
@@ -22,6 +25,22 @@ class TestBot(unittest.TestCase):
             companies_list = get_all_companies()
             expected = [('AAPL', 0), ('GOOG', 0), ('MSFT', 0)]
             self.assertEqual(companies_list, expected)
+
+    def test_check_alg(self):
+        test_cases = [(30, 'SUPER STRONG BUY'),  (0, 'SELL'), (10, 'BUY'), (-20, 'STRONG SELL')]
+        for price, expected in test_cases:
+            result = check_alg(price)
+            assert result == expected, f"Expected: {expected}, Got: {result}"
+
+    def test_alg_sell_buy(self):
+        prices, difference_price, deptht_sell, deptht_buy, rsi, expected = [100, 110, 120, 130, 140], 10, 5, 5, 70, 'BUY'
+        result = alg_sell_buy(prices, difference_price, deptht_sell, deptht_buy, rsi)
+        assert result == expected, f"Expected: {expected}, Got: {result}"
+
+    def test_own_recomendation(self):
+        action, difference_price, difference_volume, deptht_sell, deptht_buy, rsi, cci, expected = 100, 0.4, 0.4, 0.4, 100, 20, 5, 'STRONG BUY'
+        result = own_recommendetion(action, difference_price, difference_volume, deptht_sell, deptht_buy, rsi, cci)
+        assert result == expected, f"Expected: {expected}, Got: {result}"
 
 if __name__ == '__main__':
     unittest.main()
